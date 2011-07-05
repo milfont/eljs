66ac9c3d23623bd938ba31088e77375e3b6b354d
Christiano Milfont <cmilfont@gmail.com>
2011-07-05 15:47:15 -0300

    plugin to jQuery

---
+++ specs/jquery.spec.js
@@ -0,0 +1,52 @@
+describe('jQuery Plugin', function() {
+
+    var userTest, renderedTemplate;
+
+    beforeEach( function () {
+        userTest = {
+            name: "Christiano Milfont",
+            corporation: {
+                name: "Milfont Consulting",
+                address: [{
+                    country: "Brazil"
+                }]
+            }
+        };
+        renderedTemplate = "    <div><span>Christiano Milfont</span></div>    <ul>    <span>Milfont Consulting</span>    <ul>     <li>Brazil</li> </ul></ul>";
+
+        jQuery("body").html("");
+        jQuery("<div>").attr("id", "user_profile").appendTo("body");
+        //mock ajax
+        jQuery.extend({
+            ajax: function( ajax ) {
+                var fs = require('fs');
+                var templatePath = __dirname + ajax.url;
+                var template     = fs.readFileSync(templatePath).toString();
+                ajax.success(template);
+            }
+        });
+        //require plugin
+        require(__dirname + "/src/jquery.el");
+    });
+
+    it('should parser a template with jQuery', function () {
+
+        var user_profile = jQuery("#user_profile");
+
+        user_profile.render({
+            url: '/templates/nestedSprites.html'
+            , template: "user"
+            , json: {"user": userTest }
+        });
+
+        waitsFor(function(){
+            return user_profile.html() !== "";
+        }, "Rendered Template", 10000);
+
+        runs(function(){
+            expect(user_profile.html().replace(/\n/g, "")).toEqual(renderedTemplate.toString());
+        });
+
+    });
+    
+});
---
+++ src/jquery.el.js
@@ -0,0 +1,68 @@
+(function ($) {
+
+    var content = $("<div>");
+
+    var spriteLoader = function(sprite) {
+        return $(content).find("#"+sprite).html().toString();
+    };
+
+    var helpers = function() {
+        return {
+            spriteLoader: spriteLoader,
+            partial: function(collection, template) {
+
+                var partialTemplate = "";
+                if(collection.forEach) {
+                    collection.forEach(function(item){
+                        var json = {}; 
+                        json[template] = item;
+                        var nestedEljs = new Eljs({
+                            json: json,
+                            template: spriteLoader(template)
+                            , helpers: helpers()
+                        });
+                        partialTemplate = nestedEljs.parse();
+                    });
+                } else {
+                    var json = {}; 
+                    json[template] = collection;
+                    var nestedEljs = new Eljs({
+                        json: json,
+                        template: spriteLoader(template)
+                        , helpers: helpers()
+                    });
+                    partialTemplate = nestedEljs.parse();
+                }
+                return partialTemplate;
+            }
+        }
+    };
+
+    var render = function(config) {
+        return (new Eljs({ 
+            template: spriteLoader( config.template )
+            , json : config.json
+            , helpers: helpers()
+        })).parse();
+    };
+
+    $.fn.extend({
+        render: function(config) {
+            var self = this;
+            $.ajax({
+                url: config.url
+                , dataType: "html"
+                , success: function(data){
+                    content.html(data);
+                    var rendered = render(config);
+                    self.html( rendered );
+                    if(typeof config.callback === "function") {
+                        config.callback(rendered);
+                    }
+                }
+            });
+            return this;
+        }
+    });
+
+})(jQuery);
